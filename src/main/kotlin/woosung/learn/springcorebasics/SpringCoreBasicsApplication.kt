b2f8a1d80d246325a70fd6b409747b970e19f7a8
woosung-21 <woosung.21@navercorp.com>
2023-09-05 18:45:45 +0900

    initial commit

---
+++ src/main/kotlin/woosung/learn/springcorebasics/SpringCoreBasicsApplication.kt
@@ -0,0 +1,11 @@
+package woosung.learn.springcorebasics
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCoreBasicsApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringCoreBasicsApplication>(*args)
+}
---
+++ src/test/kotlin/woosung/learn/springcorebasics/SpringCoreBasicsApplicationTests.kt
@@ -0,0 +1,13 @@
+package woosung.learn.springcorebasics
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCoreBasicsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
