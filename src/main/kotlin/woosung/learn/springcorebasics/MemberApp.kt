3c3c96549c4143a6759b4c48bdfdba0ee4bf03af
woosung-21 <woosung.21@navercorp.com>
2023-09-07 11:38:14 +0900

    modified main function

--- src/main/kotlin/woosung/learn/springcorebasics/MemberApp.kt
+++ src/main/kotlin/woosung/learn/springcorebasics/MemberApp.kt
@@ -4,14 +4,12 @@ import woosung.learn.springcorebasics.member.Grade
 import woosung.learn.springcorebasics.member.Member
 import woosung.learn.springcorebasics.member.MemberServiceImpl
 
-class MemberApp {
-    fun main(args: List<String>) {
-        val memberService = MemberServiceImpl()
-        val member = Member(1L, "A", Grade.VIP)
-        memberService.join(member)
+fun main() {
+    val memberService = MemberServiceImpl()
+    val member = Member(1L, "A", Grade.VIP)
+    memberService.join(member)
 
-        val findMember = memberService.findMember(1L)
-        println("new member = ${member.name}")
-        println("find member = ${findMember?.name}")
-    }
+    val findMember = memberService.findMember(1L)
+    println("new member = ${member.name}")
+    println("find member = ${findMember?.name}")
 }
