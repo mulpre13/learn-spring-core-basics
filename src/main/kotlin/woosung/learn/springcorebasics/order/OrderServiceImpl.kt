ee66639fb169d2ea597c5f814b942aff3b77bad6
woosung-21 <woosung.21@navercorp.com>
2023-09-10 17:02:41 +0900

    applied rate discount policy

--- src/main/kotlin/woosung/learn/springcorebasics/order/OrderServiceImpl.kt
+++ src/main/kotlin/woosung/learn/springcorebasics/order/OrderServiceImpl.kt
@@ -1,12 +1,12 @@
 package woosung.learn.springcorebasics.order
 
-import woosung.learn.springcorebasics.discount.FixedDiscountPolicy
+import woosung.learn.springcorebasics.discount.RateDiscountPolicy
 import woosung.learn.springcorebasics.member.Member
 import woosung.learn.springcorebasics.member.MemoryMemberRepository
 
 class OrderServiceImpl : OrderService {
     private val memberRepository = MemoryMemberRepository()
-    private val discountPolicy = FixedDiscountPolicy()
+    private val discountPolicy = RateDiscountPolicy()
 
     override fun createOrder(memberId: Long, itemName: String, itemPrice: Int): Order {
         val member: Member = memberRepository.findById(memberId)!!
