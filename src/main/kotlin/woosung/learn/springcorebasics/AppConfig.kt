b76cf5b17842a2e94588bd96f52a5b194a4f66f8
woosung-21 <woosung.21@navercorp.com>
2023-09-10 18:06:28 +0900

    refactored AppConfig to designate impls for all classe

--- src/main/kotlin/woosung/learn/springcorebasics/AppConfig.kt
+++ src/main/kotlin/woosung/learn/springcorebasics/AppConfig.kt
@@ -1,6 +1,8 @@
 package woosung.learn.springcorebasics
 
+import woosung.learn.springcorebasics.discount.DiscountPolicy
 import woosung.learn.springcorebasics.discount.RateDiscountPolicy
+import woosung.learn.springcorebasics.member.MemberRepository
 import woosung.learn.springcorebasics.member.MemberService
 import woosung.learn.springcorebasics.member.MemberServiceImpl
 import woosung.learn.springcorebasics.member.MemoryMemberRepository
@@ -8,6 +10,8 @@ import woosung.learn.springcorebasics.order.OrderService
 import woosung.learn.springcorebasics.order.OrderServiceImpl
 
 class AppConfig {
-    fun memberService(): MemberService = MemberServiceImpl(MemoryMemberRepository())
-    fun orderService(): OrderService = OrderServiceImpl(MemoryMemberRepository(), RateDiscountPolicy())
+    fun memberService(): MemberService = MemberServiceImpl(memberRepository())
+    fun orderService(): OrderService = OrderServiceImpl(memberRepository(), discountPolicy())
+    fun memberRepository(): MemberRepository = MemoryMemberRepository()
+    fun discountPolicy(): DiscountPolicy = RateDiscountPolicy()
 }
