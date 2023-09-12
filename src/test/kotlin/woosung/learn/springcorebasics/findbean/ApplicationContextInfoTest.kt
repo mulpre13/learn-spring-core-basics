45c165f9a86c4be7fd53c6ec0d0e1ef420e131a0
woosung-21 <woosung.21@navercorp.com>
2023-09-12 19:52:52 +0900

    added getbean tests

--- src/test/kotlin/woosung/learn/springcorebasics/findbean/ApplicationContextInfoTest.kt
+++ src/test/kotlin/woosung/learn/springcorebasics/findbean/ApplicationContextInfoTest.kt
@@ -1,9 +1,13 @@
 package woosung.learn.springcorebasics.findbean
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.types.shouldBeInstanceOf
+import org.springframework.beans.factory.NoSuchBeanDefinitionException
 import org.springframework.beans.factory.config.BeanDefinition.ROLE_APPLICATION
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import woosung.learn.springcorebasics.AppConfig
+import woosung.learn.springcorebasics.member.MemberService
 
 class ApplicationContextInfoTest : FunSpec() {
     init {
@@ -18,6 +22,24 @@ class ApplicationContextInfoTest : FunSpec() {
             val beanNames = appConfig.beanDefinitionNames
             beanNames.filter { appConfig.getBeanDefinition(it).role == ROLE_APPLICATION }.forEach {
                 println("name: $it, object: ${appConfig.getBean(it)}")
+            }
+        }
+
+        test("find bean by name") {
+            val memberService: MemberService = appConfig.getBean("memberService", MemberService::class.java)
+            memberService.shouldBeInstanceOf<MemberService>()
+        }
+
+        test("find bean by type") {
+            val memberService: MemberService = appConfig.getBean(MemberService::class.java)
+            memberService.shouldBeInstanceOf<MemberService>()
+        }
+
+        test("unable to find bean by name") {
+            shouldThrow<NoSuchBeanDefinitionException> {
+                appConfig.getBean(
+                    "unexisting bean", MemberService::class.java
+                )
             }
         }
     }
